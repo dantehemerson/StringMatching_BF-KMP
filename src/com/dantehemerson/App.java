21b2bd690a9ce6201fea6f6ae098ed2848672066
dantehemerson <dantehemerson@gmail.com>
2017-07-23 01:03:22 +0000

    Initial commit

---
+++ src/com/dantehemerson/App.java
@@ -0,0 +1,31 @@
+package com.dantehemerson;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Dante on 22/07/2017.
+ */
+public class App {
+    private JPanel mainPanel;
+    private JButton button1;
+    private JTextField ingreseseElTextoTextField;
+    private JComboBox comboBox1;
+    private JTextArea textArea1;
+
+    public App() {
+
+
+
+    }
+
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/com/dantehemerson/Main.java
@@ -0,0 +1,19 @@
+package com.dantehemerson;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Dante on 22/07/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("String Matching(BF - KMP)");
+        frame.setContentPane(new App().getMainPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(800, 350));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
